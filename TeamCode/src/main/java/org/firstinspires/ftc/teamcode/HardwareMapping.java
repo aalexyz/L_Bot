90725b1fb3d0c34caa8a56ca047f962bc0538e8a
aalexyz <90709761+aalexyz@users.noreply.github.com>
2024-01-18 16:15:31 +0200

    Did stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMapping.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMapping.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
 
 public class HardwareMapping {
     public static final String FRONT_LEFT_MOTOR = "motorFL";
@@ -10,18 +12,33 @@ public class HardwareMapping {
     public static final String BACK_RIGHT_MOTOR = "motorBR";
 
     public final DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor;
+    public final IMU imu;
 
-    public HardwareMapping(DcMotor frontLeftMotor, DcMotor backLeftMotor, DcMotor frontRightMotor, DcMotor backRightMotor) {
+    public HardwareMapping(DcMotor frontLeftMotor, DcMotor backLeftMotor, DcMotor frontRightMotor, DcMotor backRightMotor, IMU imu) {
         this.frontLeftMotor = frontLeftMotor;
         this.backLeftMotor = backLeftMotor;
         this.frontRightMotor = frontRightMotor;
         this.backRightMotor = backRightMotor;
+        this.imu = imu;
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     public static HardwareMapping from(HardwareMap map) {
         return new HardwareMapping(map.dcMotor.get(FRONT_LEFT_MOTOR),
                 map.dcMotor.get(BACK_LEFT_MOTOR),
                 map.dcMotor.get(FRONT_RIGHT_MOTOR),
-                map.dcMotor.get(BACK_RIGHT_MOTOR));
+                map.dcMotor.get(BACK_RIGHT_MOTOR), map.get(IMU.class, "imu"));
     }
 }
